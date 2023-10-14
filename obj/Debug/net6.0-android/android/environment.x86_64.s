4229534db4fa1a91b97e7952692a595f1704f359
Anthony <ossipeerivermediagroup@outlook.com>
2023-10-14 01:31:13 -0400

    changed flyout options to match future map views: Topo, satalite, Forecast

--- obj/Debug/net6.0-android/android/environment.x86_64.s
+++ obj/Debug/net6.0-android/android/environment.x86_64.s
@@ -3702,7 +3702,7 @@ dso_cache:
 	.size	dso_cache, 1024
 
 	#
-	# Generated from instance of: Xamarin.Android.Tasks.ApplicationConfig, Xamarin.Android.Build.Tasks, Version=12.3.99.76, Culture=neutral, PublicKeyToken=84e04ff9cfb79065
+	# Generated from instance of: Xamarin.Android.Tasks.ApplicationConfig, Xamarin.Android.Build.Tasks, Version=12.3.99.109, Culture=neutral, PublicKeyToken=84e04ff9cfb79065
 	#
 	.type	application_config, @object
 	.global	application_config
@@ -3778,7 +3778,7 @@ application_config:
 
 	.type	.L.autostr.8, @object
 .L.autostr.8:
-	.asciz	"a5b66670-55ba-45f3-90a3-7a1bbf441435"
+	.asciz	"6e7ff0f1-757a-4e88-a6d0-e72aa6ed888e"
 	.size	.L.autostr.8, 37
 
 	.type	.L.autostr.9, @object
@@ -3967,4 +3967,4 @@ application_config:
 	.size	.L.autostr.45, 25
 
 
-	.ident	"Xamarin.Android remotes/origin/release/6.0.4xx @ af4a25d50f5a602c77d05c2ec8c8644c00cffbbd"
+	.ident	"Xamarin.Android remotes/origin/release/6.0.4xx @ 25cb50d78ac929b03d2ec6f1a4c0a7aeb9bcebc3"
