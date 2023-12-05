15bcf569eac48904f9c35c0d739d3e66bb8c4c2f
Anthony <98359099+AjSmith850@users.noreply.github.com>
2023-12-05 16:37:02 -0500

    Add files via upload

--- Views/SavedLocationsPage.xaml.cs
+++ Views/SavedLocationsPage.xaml.cs
@@ -13,7 +13,6 @@ public partial class SavedLocationsPage : ContentPage
 
         MainMapView.PropertyChanged += (object sender, PropertyChangedEventArgs e) =>
         {
-            // The map view's location display is initially null, so check for a location display property change before enabling it.
             if (e.PropertyName == nameof(LocationDisplay))
             {
                 _ = DisplayDeviceLocationAsync();
@@ -24,9 +23,29 @@ public partial class SavedLocationsPage : ContentPage
     private async Task DisplayDeviceLocationAsync()
     {
         PermissionStatus status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-
         MainMapView.LocationDisplay.IsEnabled = status == PermissionStatus.Granted || status == PermissionStatus.Restricted;
         MainMapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Recenter;
     }
+
+    private async void SaveCurrentLocationToViewModel(object sender, EventArgs e)
+    {
+        var location = await Geolocation.GetLastKnownLocationAsync();
+        if (location != null)
+        {
+            // Display an alert dialog with an input field
+            string locationName = await DisplayPromptAsync("Save Location", "Enter location name:", initialValue: "", maxLength: 50, keyboard: Keyboard.Text);
+
+            if (!string.IsNullOrWhiteSpace(locationName))
+            {
+                double latitude = location.Latitude;
+                double longitude = location.Longitude;
+
+                var viewModel = BindingContext as SavedLocationsViewModel;
+                viewModel?.SaveCurrentLocation(latitude, longitude, locationName);//
+            }
+        }
+    }
+
+
 }
 
